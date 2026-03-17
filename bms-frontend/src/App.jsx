8e98bffd3bf63c1e33621ac1d28d412ee5b35a28
Supam Roy <supamroy8@gmail.com>
2026-03-17 18:30:06 +0530

    new

--- bms-frontend/src/App.jsx
+++ bms-frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <h1 className="text-center text-4xl mt-10 underline">Welcome to BookMyScreen</h1>
+     <div>
+      <main>
+        <Routes>
+          {/* Define your routes here */}
+           <Route path="/" element={<Home page />} /> 
+          <Route path="/profile/:id" element={<h1>profile page </h1>} /> 
+        </Routes>
+      </main>
+     </div>
     </>
   )
 }
